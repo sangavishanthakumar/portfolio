152908659095a6af83c39f5ad17e05eb14cbdbf2
Sanny <s@t.de>
2023-05-03 20:03:19 +0200

    navbar added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,40 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {RouterModule, Routes} from "@angular/router";
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
+import { SkillsetComponent } from './skillset/skillset.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
+import { ErrorComponent } from './error/error.component';
 
+
+const appRoute: Routes = [
+  /*{path: '', component: HomeComponent},*/
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'aboutme', component: AboutmeComponent },
+  { path: 'skillset', component: SkillsetComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '**', component: ErrorComponent } /* always last */
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutmeComponent,
+    SkillsetComponent,
+    ProjectsComponent,
+    ContactComponent,
+    ErrorComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+      RouterModule.forRoot(appRoute)
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/skillset/skillset.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-skillset',
+  templateUrl: './skillset.component.html',
+  styleUrls: ['./skillset.component.css']
+})
+export class SkillsetComponent {
+
+}
