dbeb8395270507727136474427d7decbfe0048e1
Sangavi Shanthakumar <92029067+sangavishanthakumar@users.noreply.github.com>
2024-02-09 13:39:51 +0100

    routing adjustment; updated contact css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
+import { filter, map, mergeMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'portfolio';
   showMenu: boolean | undefined;
+
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+      map(() => this.activatedRoute),
+      map(route => {
+        while (route.firstChild) route = route.firstChild;
+        return route;
+      }),
+      filter(route => route.outlet === 'primary'),
+      mergeMap(route => route.fragment)
+    ).subscribe(fragment => this.scrollTo(fragment));
+  }
 
   toggleMenu() {
     this.showMenu = !this.showMenu;
   }
 
-  scrollTo(element: string): void {
-    const el = document.querySelector(element);
-    // @ts-ignore
-    el.scrollIntoView({ behavior: 'smooth' });
+  scrollTo(elementId: string | null): void {
+    if (elementId) {
+      const el = document.querySelector(`#${elementId}`);
+      if (el) {
+        el.scrollIntoView({ behavior: 'smooth' });
+      }
+    }
   }
-
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,27 @@
-import {Component} from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        setTimeout(() => this.scrollToSection(window.location.pathname.replace('/', '')), 100);
+      }
+    });
+  }
+
+  scrollToSection(section: string) {
+    const el = document.querySelector(`#${section}`);
+    if (el) {
+      el.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
 }
