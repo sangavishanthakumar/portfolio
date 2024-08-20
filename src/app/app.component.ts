1038f784fdd01bda6e5b129e322297b1ce750c3d
Sangavi Shanthakumar <92029067+sangavishanthakumar@users.noreply.github.com>
2024-08-20 13:39:14 +0200

    nav closes in mobile view; removed scrollTo method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,41 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
-import { filter, map, mergeMap } from 'rxjs/operators';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'portfolio';
   showMenu: boolean | undefined;
-
-  constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
-
-  ngOnInit() {
-    this.router.events.pipe(
-      filter(event => event instanceof NavigationEnd),
-      map(() => this.activatedRoute),
-      map(route => {
-        while (route.firstChild) route = route.firstChild;
-        return route;
-      }),
-      filter(route => route.outlet === 'primary'),
-      mergeMap(route => route.fragment)
-    ).subscribe(fragment => this.scrollTo(fragment));
-  }
 
   toggleMenu() {
     this.showMenu = !this.showMenu;
   }
 
-  scrollTo(elementId: string | null): void {
-    if (elementId) {
-      const el = document.querySelector(`#${elementId}`);
-      if (el) {
-        el.scrollIntoView({ behavior: 'smooth' });
-      }
+  closeMenu() {
+    if (this.showMenu) {
+      this.showMenu = false;
     }
   }
 }
